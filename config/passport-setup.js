9f9f2c1c6d97f0ac5fb60f950b718dc76c27d53f
kenneth maberi <kennethmaberi@gmail.com>
2025-12-01 12:58:55 +0300

    tre

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,12 +1,6 @@
 const passport = require('passport');
 const GitHubStrategy = require('passport-github2').Strategy;
 
-app.use(session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false
-}));
-
 passport.serializeUser((user, done) => {
     done(null, user.id);
 });
