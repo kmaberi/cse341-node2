4ca078984cd53d30e4df3cb770dcaeb0103d07c9
kenneth maberi <kennethmaberi@gmail.com>
2025-12-04 11:26:32 +0300

    gfd

--- middleware/validation.js
+++ middleware/validation.js
@@ -31,7 +31,7 @@ const validate = (req, res, next) => {
     const extractedErrors = [];
     errors.array().map(err => extractedErrors.push({ [err.param]: err.msg }));
 
-    return res.status(422).json({
+    return res.status(400).json({
         errors: extractedErrors,
     });
 };
